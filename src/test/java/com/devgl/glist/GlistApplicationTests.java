21ec9ba94e2823a22dd7ab86e4844136f07d3700
Glaysonluiz1 <Glaysonluiz1@gmail.com>
2023-11-13 21:21:18 -0300

    Projeto criado

---
+++ src/test/java/com/devgl/glist/GlistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devgl.glist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
